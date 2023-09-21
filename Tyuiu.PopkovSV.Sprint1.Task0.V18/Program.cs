6439a3c5c420564fedeb4eb3bacbc20880de41d7
Tyuiu.PopkovSV <popkovsergej10@gmail.com>
2023-09-21 23:16:52 +0300

    Add project files.

---
+++ Tyuiu.PopkovSV.Sprint1.Task0.V18/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.PopkovSV.Sprint1.Task0.V18.Lib;
+
+// ЗАДАНИЕ
+//Написать программу, которая вычиссляет выражение 9 * (6 / 2 * 3) / 9 / 3 и печатает результат на экране
+
+namespace Tyuiu.PopkovSV.Sprint1.Task0.V18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт#1 | Выполнил: Попков С. В. | ИСТНб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт#1                                                                *");
+            Console.WriteLine("* Тема: Базовые навыки рааботы в C#                                       *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #18                                                             *");
+            Console.WriteLine("* Выполнил: Попков Сергей Вадимович | ИСТНб-23-1                          *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычиссляет выражение 9 * (6 / 2 * 3) / 9 / 3*");
+            Console.WriteLine("* и печатает результат на экране.                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                         ");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* 9 * (6 / 2 * 3) / 9 / 3                                                 *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*РЕЗУЛЬТАТ:                                                               *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.Calculate());
+
+            Console.ReadLine();
+        }
+    }
+}
