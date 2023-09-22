f2900e43afeeba85568bbf6275e23ad55f58d9dd
Tyuiu.PopkovSV <popkovsergej10@gmail.com>
2023-09-22 18:35:06 +0300

    Task1

---
+++ Tyuiu.PopkovSV.Sprint1.Task1.V20.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.PopkovSV.Sprint1.Task1.V20.Lib;
+
+namespace Tyuiu.PopkovSV.Sprint1.Task1.V20.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 1.0;
+            double y = 2.0;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(11, res);
+
+        }
+    }
+}
---
+++ Tyuiu.PopkovSV.Sprint1.Task1.V20.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.PopkovSV.Sprint1.Task1.V20.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.PopkovSV.Sprint1.Task1.V20.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3233fa50-8ead-4276-96bf-82e212bac458")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
