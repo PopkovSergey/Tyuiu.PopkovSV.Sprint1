699d72d817ad446a2ccbb9748db201cdcc556c9f
Tyuiu.PopkovSV <popkovsergej10@gmail.com>
2023-09-23 21:14:44 +0300

    Task4

---
+++ Tyuiu.PopkovSV.Sprint1.Task4.V20.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.PopkovSV.Sprint1.Task4.V20.Lib;
+
+namespace Tyuiu.PopkovSV.Sprint1.Task4.V20.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 2;
+            double y = 7;
+            double wait = 3;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
