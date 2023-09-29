6ceb9fcb0cbd7813e2b79550cf9c433d54a1e708
Tyuiu.PopkovSV <popkovsergej10@gmail.com>
2023-09-29 21:59:09 +0300

    Task7

---
+++ Tyuiu.PopkovSV.Sprint1.Task7.V2.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.PopkovSV.Sprint1.Task7.V2.Lib;
+
+namespace Tyuiu.PopkovSV.Sprint1.Task7.V2.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 1;
+            double y = 2;
+            double wait = 2.519;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.PopkovSV.Sprint1.Task7.V2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.PopkovSV.Sprint1.Task7.V2.Lib;
+
+namespace Tyuiu.PopkovSV.Sprint1.Task7.V2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Попков С. В. | ИСТНб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт#1                                                                *");
+            Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                 *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #2                                                              *");
+            Console.WriteLine("* Выполнил: Попков Сергей Вадимович | ИСТНб-23-1                          *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, печатает все слова, удалив среднюю букву            *");
+            Console.WriteLine("* у слов нечетной длины.                                                  *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ((sin(x) + cos(y)) / (cos(x) - sin(y))) * tg(xy)                        *");
+
+            double x, y;
+
+            Console.WriteLine("Введите значение X");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите значение Y");
+            y = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.Calculate(x, y));
+
+            Console.ReadKey();
+        }
+    }
+}
